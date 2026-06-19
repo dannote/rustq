51b8e63725d0f170e3d1c15e40466bd6cd0c178b
Danila Poyarkov <dev@dannote.net>
2026-06-19 09:58:36 +0200

    Guard centralized Rust path splitting

--- test/rustq/architecture_test.exs
+++ test/rustq/architecture_test.exs
@@ -3,6 +3,19 @@ defmodule RustQ.ArchitectureTest do
 
   @source_paths Path.wildcard("lib/**/*.ex") ++ Path.wildcard("test/**/*.exs")
 
+  test "Rust path string splitting stays centralized" do
+    allowed = MapSet.new(["lib/rustq/rust/ast/builder.ex", "lib/rustq/rust/ast/type_builder.ex"])
+
+    path_split = ~r/String\.split\([^\n]*\"::\"/
+
+    offenders =
+      @source_paths
+      |> Enum.reject(&MapSet.member?(allowed, &1))
+      |> Enum.filter(&(File.read!(&1) =~ path_split))
+
+    assert offenders == []
+  end
+
   test "removed API and boilerplate patterns do not return" do
     source = Enum.map_join(@source_paths, "\n", &File.read!/1)
 
