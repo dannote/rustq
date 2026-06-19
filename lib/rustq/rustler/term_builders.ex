fc59ee1bdb504c8b77c22aba3945f836331029ee
Danila Poyarkov <dev@dannote.net>
2026-06-19 15:49:41 +0200

    Dogfood term builders with defrust

--- lib/rustq/rustler/term_builders.ex
+++ lib/rustq/rustler/term_builders.ex
@@ -1,43 +1,54 @@
 defmodule RustQ.Rustler.TermBuilders do
   @moduledoc false
 
-  use RustQ.Sigil
+  use RustQ.Meta
 
   alias RustQ.Rust
+  alias RustQ.Type, as: R
 
   @names [:map_from_terms, :struct_from_terms]
-
-  @templates %{
-    map_from_terms: ~R"""
-    fn make_map_from_terms<'a>(
-        env: Env<'a>,
-        pairs: &[(Term<'a>, Term<'a>)],
-    ) -> NifResult<Term<'a>> {
-        let (keys, values): (Vec<_>, Vec<_>) = pairs.iter().copied().unzip();
-        Term::map_from_term_arrays(env, &keys, &values)
-    }
-    """,
-    struct_from_terms: ~R"""
-    fn make_struct_from_terms<'a>(
-        env: Env<'a>,
-        keys: &[Term<'a>],
-        values: &[Term<'a>],
-    ) -> NifResult<Term<'a>> {
-        Term::map_from_term_arrays(env, keys, values)
-    }
-    """
+  @function_names %{
+    map_from_terms: :make_map_from_terms,
+    struct_from_terms: :make_struct_from_terms
   }
+
+  @spec make_map_from_terms(R.path(:Env, R.lifetime(:a)), R.slice({R.term(), R.term()})) ::
+          R.nif_result(R.term())
+  defrust make_map_from_terms(env, pairs) do
+    keys = Vec.with_capacity(pairs.len())
+    values = Vec.with_capacity(pairs.len())
+
+    for {key, value} <- pairs.iter().copied() do
+      keys.push(key)
+      values.push(value)
+    end
+
+    Term.map_from_term_arrays(env, ref(keys), ref(values))
+  end
+
+  @spec make_struct_from_terms(R.path(:Env, R.lifetime(:a)), R.slice(R.term()), R.slice(R.term())) ::
+          R.nif_result(R.term())
+  defrust make_struct_from_terms(env, keys, values) do
+    Term.map_from_term_arrays(env, keys, values)
+  end
 
   @spec build(keyword()) :: [Rust.Fragment.t()]
   def build(opts \\ []) do
     opts
     |> Keyword.get(:include, @names)
     |> include_names()
-    |> Enum.map(&Rust.item(RustQ.render!(template!(&1), "rustler_term_builder.rs")))
+    |> Enum.map(&rusty_item/1)
   end
 
   defp include_names(:all), do: @names
   defp include_names(names), do: List.wrap(names)
 
-  defp template!(name), do: Map.fetch!(@templates, name)
+  defp rusty_item(name) do
+    function_name = Map.fetch!(@function_names, name)
+
+    __rustq_asts__()
+    |> Enum.find(&(&1.name == function_name))
+    |> RustQ.Rust.AST.Render.render_item()
+    |> Rust.item()
+  end
 end
